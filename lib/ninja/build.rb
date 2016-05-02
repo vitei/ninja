0b882a94b5ab30317469982f8efd1377e13bceeb
Daniel Nagase <nagase@vitei.com>
2016-05-02 11:14:04 +0900

    Add the ability to store dependencies for build statements.
    We provide support for both implicit and order-only dependencies. I
    don't have a good idea of how to validate these (they will basically
    be paths, so anything that you can define for a variable value should
    be accepted), so for now I'm just making sure they're not nil.

--- lib/ninja/build.rb
+++ lib/ninja/build.rb
@@ -3,7 +3,9 @@ module Ninja
     attr_reader :rule,
                 :inputs,
                 :output,
-                :variables
+                :variables,
+                :implicit_deps,
+                :order_only_deps
 
     def initialize(desc={})
       Description.validate!(desc)
@@ -12,6 +14,8 @@ module Ninja
       @inputs = [*desc[:inputs]]
       @output = desc[:output]
       @variables = desc.fetch(:variables, {})
+      @implicit_deps = desc.fetch(:implicit_deps, {})
+      @order_only_deps = desc.fetch(:order_only_deps, {})
     end
 
     module Description #:nodoc:
@@ -30,6 +34,22 @@ module Ninja
          if desc.has_key?(:variables)
            desc[:variables].each do |name, value|
              raise "Expected name to be a string made of [a-Z,_,-,0-9] characters." unless /\w/.match(name)
+           end
+         end
+
+         if desc.has_key?(:implicit_deps)
+           raise "Expected implicit dependencies." unless ! desc[:implicit_deps].empty?
+
+           desc[:implicit_deps].each do |dep|
+             raise "Expected implicit dependencies to be non-nil" unless dep != nil
+           end
+         end
+
+         if desc.has_key?(:order_only_deps)
+           raise "Expected order only dependencies." unless ! desc[:order_only_deps].empty?
+
+           desc[:order_only_deps].each do |dep|
+             raise "Expected order only dependencies to be non-nil" unless dep != nil
            end
          end
       end
